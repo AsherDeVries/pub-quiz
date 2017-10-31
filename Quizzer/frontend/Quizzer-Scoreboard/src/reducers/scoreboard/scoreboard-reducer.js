5fa527110c8e0ac5a0edc318dddfe105d1e6367d
Nick van der Burg <nickvdburg.nvdb@gmail.com>
2017-10-31 14:38:50 +0100

    change scoreboard reducer

--- Quizzer/frontend/Quizzer-Scoreboard/src/reducers/scoreboard/scoreboard-reducer.js
+++ Quizzer/frontend/Quizzer-Scoreboard/src/reducers/scoreboard/scoreboard-reducer.js
@@ -1,18 +1,5 @@
 import initialState from './initial-scoreboard-state';
 
 export default function scoreboardReducer(state = initialState, action) {
-
   return state;
-  /*switch (action.type) {
-    case(SESSION_ACTION_TYPES.LOGIN_REQUEST_STATE):
-      return {...state,
-        loginState: action.loginState
-      };
-    case(SESSION_ACTION_TYPES.LOGIN_STATUS_CHANGE):
-      return {...state,
-        isLoggedIn: action.isLoggedIn
-      };
-    default:
-      return state;
-  }*/
 }

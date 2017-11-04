ef67225384356ae0500cd768987fe5af727ac118
Asher <asher.devries@gmail.com>
2017-11-04 13:14:41 +0100

    modified seedscript

--- Quizzer/backend/Quizzer-Server/dist/database/db.js
+++ Quizzer/backend/Quizzer-Server/dist/database/db.js
@@ -10,21 +10,19 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 
 var _config = require('../config.json');
 
-var _categories = require('../assets/categories');
+var _seedScript = require('./seed-script');
 
-var _categories2 = _interopRequireDefault(_categories);
-
-var _Question = require('../models/Question');
-
-var _Question2 = _interopRequireDefault(_Question);
+var _seedScript2 = _interopRequireDefault(_seedScript);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function () {
-  var db = void 0;
-  _mongoose2.default.connect('mongodb://localhost:' + _config.db_port + '/' + _config.db_name, { useMongoClient: true }, function (err, result) {
-    db = result;
-    _Question2.default.populate(_categories2.default);
+  _mongoose2.default.Promise = global.Promise;
+  var options = { useMongoClient: true, promiseLibrary: global.Promise };
+  _mongoose2.default.connect('mongodb://localhost:' + _config.db_port + '/' + _config.db_name, options, function (err, db) {
+    if (db) {
+      (0, _seedScript2.default)();
+    }
   });
 };
 //# sourceMappingURL=db.js.map
--- Quizzer/backend/Quizzer-Server/dist/websockets/start-quiznight/signup-quizmaster.js
+++ Quizzer/backend/Quizzer-Server/dist/websockets/start-quiznight/signup-quizmaster.js
@@ -12,14 +12,6 @@ var _Quiznight = require('../../models/Quiznight');
 
 var _Quiznight2 = _interopRequireDefault(_Quiznight);
 
-var _randomstring = require('randomstring');
-
-var _randomstring2 = _interopRequireDefault(_randomstring);
-
-var _nonQuizmaster = require('../non-quizmaster');
-
-var _nonQuizmaster2 = _interopRequireDefault(_nonQuizmaster);
-
 var _app = require('../../app');
 
 var _app2 = _interopRequireDefault(_app);
@@ -30,35 +22,5 @@ var _message_types2 = _interopRequireDefault(_message_types);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (socket) {
-  socket.on(_message_types2.default.CONNECT_QUIZMASTER, function (credentials) {
-    if (credentials.username === _config2.default.quizmaster_credentials.username && credentials.password === _config2.default.quizmaster_credentials.password) {
-      var quiznightCode = _randomstring2.default.generate({
-        length: 6,
-        charset: 'alphanumeric'
-      });
-      var qn = new _Quiznight2.default({
-        _id: quiznightCode,
-        teams: [],
-        rounds: []
-      });
-
-      qn.save(function (err) {
-        if (err) {
-          socket.emit(_message_types2.default.PENDING, 'A quiznight could not be created because of the following reason: ' + quiznightCode);
-        } else {
-          var nsp = (0, _nonQuizmaster2.default)(quiznightCode);
-<<<<<<< Updated upstream
-          socket.join('QUIZMASTER_ROOM');
-          socket.emit(_message_types2.default.PENDING, 'Welcome quizmaster!, a quiznight has been created with the code: ' + quiznightCode);
-=======
-          socket.emit('PENDING', 'Welcome quizmaster!, a quiznight has been created with the code: ' + quiznightCode);
->>>>>>> Stashed changes
-        }
-      });
-    } else {
-      socket.emit(_message_types2.default.SIGN_UP_ERROR, 'Wrong credentials');
-    }
-  });
-};
+exports.default = function (socket) {};
 //# sourceMappingURL=signup-quizmaster.js.map

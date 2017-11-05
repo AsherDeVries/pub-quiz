b2263fa15c9350074516c43ad387991395eba769
Nick van der Burg <nickvdburg.nvdb@gmail.com>
2017-11-05 18:27:45 +0100

    add quiznight validation using save method

--- Quizzer/backend/Quizzer-Server/test/mongoose-models.js
+++ Quizzer/backend/Quizzer-Server/test/mongoose-models.js
@@ -7,25 +7,47 @@ import mongoose from 'mongoose';
 import Quiznight from '../src/models/Quiznight';
 import Question from '../src/models/Question';
 
-/*
-NO VALIDATION NECESSARY FOR CATEGORY AS IT ONLY CONTAINS AN _id FIELD
-*/
-/*
-UNABLE TO VALIDATE QUIZNIGHTS DUE TO MONGOOSE'S INABILITY TO VALIDATE EMBEDDED DOCUMENTS
-*/
-
+/* NO VALIDATION NECESSARY FOR CATEGORY AS IT ONLY CONTAINS AN _id FIELD */
 describe('Mongoose model tests', () => {
-/*
+
+  before('Connect to Quizzer database', () => {
+    mongoose.connect(`mongodb://localhost:${db_port}/${db_name}`);
+  });
+
+  after('Close database connection', () => {
+    mongoose.connection.close();
+  });
+
   describe('Quiznight model', () => {
     it('Should be invalid if all fields are empty', (done) => {
+
       let qn = new Quiznight();
 
-      qn.validate((err) => {
+      qn.save((err) => {
+        expect(err).to.exist;
+        done();
+      });
+    });
+    it('Should be invalid if there are no rounds', (done) => {
+      let qnNoRounds = {
+        _id: "abcd1234",
+        teams: [
+          {
+            _id: "Nano peasants",
+            roundPoints: 0
+          }
+        ]
+      }
 
+      let qn = new Quiznight({});
+
+      qn.save((err) => {
+        expect(err).to.exist;
+        done();
       });
     });
   });
-*/
+
   describe('Question model', () => {
     it('should be invalid if all fields are empty', (done) => {
       let q = new Question();
